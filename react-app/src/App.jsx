52cc81702af31d6d39b87fe2700144a08b3a60ca
Dustin Seger <dustin.seger@hotmail.com>
2021-04-16 18:50:17 -0400

    Add basic UI for starting/stopping facial recognition

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -10,6 +10,9 @@ const referenceImagePaths = [
 ]
 
 function App() {
+  const [videoPlaying, setVideoPlaying] = React.useState(true)
+  const [recPlaying, setRecPlaying] = React.useState(false)
+
   const bodyStyle = {
     margin: 0,
     padding: 0,
@@ -18,15 +21,41 @@ function App() {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
+    flexWrap: 'wrap',
+    alignContent: 'flex-start',
+  }
+
+  const onStartClick = () => {
+    setRecPlaying(true)
+  }
+  const onStopClick = () => {
+    setRecPlaying(false)
   }
 
   return (<>
     <div style={bodyStyle}>
-      <FaceRecVideo
-        referenceImagePaths={referenceImagePaths}
-        height="560px"
-        width="720px"
-      />
+      <div style={{
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems:' center',
+      }}>
+        <FaceRecVideo
+          referenceImagePaths={referenceImagePaths}
+          height="560px"
+          width="720px"
+          videoPlaying={videoPlaying}
+          recPlaying={recPlaying}
+        />
+      </div>
+      <div>
+        <button onClick={onStartClick}>
+          Start
+        </button>
+        <button onClick={onStopClick}>
+          Stop
+        </button>
+      </div>
     </div>
   </>);
 }
