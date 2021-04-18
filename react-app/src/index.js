e888dde53d7e9dfebbceae64e6143d8550b8ccee
Dustin Seger <dustin.seger@hotmail.com>
2021-04-18 14:09:13 -0400

    Import material ui libraries with basic CSS

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,14 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<>
-        <script defer src="./face-api.min.js"></script>
-                  <React.StrictMode>
-                    <App />
-                  </React.StrictMode>
-  </>,
-  document.getElementById('root')
-);
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+ReactDOM.render((
+  <React.StrictMode>
+    <CssBaseline>
+      <App/>
+    </CssBaseline>
+  </React.StrictMode>
+), document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
