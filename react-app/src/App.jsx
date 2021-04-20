214a82d4cd5703197b992295153e3b34f3535628
Dustin Seger <dustin.seger@hotmail.com>
2021-04-20 18:12:29 -0400

    Add info dialog containing meta data about the site

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import './App.css';
 import FaceRecVideoPlayer from './components/FaceRecVideoPlayer'
+import InfoDialog from './components/InfoDialog'
+
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Typography,
+} from '@material-ui/core'
+import HelpIcon from '@material-ui/icons/Help'
 
 function App() {
   const bodyStyle = {
@@ -14,12 +23,38 @@ function App() {
     alignItems: 'center',
   }
 
+  const [infoDialogOpen, setInfoDialogOpen] = React.useState(false)
+
+  const openInfoDialog = React.useCallback(() => {
+    setInfoDialogOpen(true)
+  }, [setInfoDialogOpen])
+
+  const onInfoDialogClose = React.useCallback(() => {
+    setInfoDialogOpen(false)
+  }, [setInfoDialogOpen])
+
   return (<>
     <div style={bodyStyle}>
+      <AppBar position="absolute">
+        <Toolbar>
+          <Typography variant="h5" style={{ flexGrow: 1 }}>
+            Live Facial Recognition
+          </Typography>
+          <IconButton style={{color: 'white'}} size="large">
+            <HelpIcon fontSize="large" onClick={openInfoDialog}/>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+
       <div>
         <FaceRecVideoPlayer/>
       </div>
     </div>
+    <InfoDialog
+      open={infoDialogOpen}
+      onClose={onInfoDialogClose}
+      maxWidth="sm"
+    />
   </>);
 }
 
